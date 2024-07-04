b868dd1b678cf416f023cb31f3367893dfb51143
Mounirou Abdul <mounirouabdul40@gmail.com>
2024-07-04 12:56:00 +0200

    update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,36 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Main() {
   const Navigate = useNavigate()
   const [user, setUser] = useState({})
+  const apiUrl = process.env.REACT_APP_API_URL
 
 
   useEffect(()=>{
     if(!localStorage.token) Navigate('/login')
   })
+
+  useEffect(()=>{
+
+    const getUser = async()=>{
+        // console.log(localStorage.getItem('admin_token'));
+        await axios.get(`http://${apiUrl || "localhost"}:5000/api/users/`,{
+            headers:{
+                token: localStorage.getItem('token')
+            }
+        }).then(response => {
+          setUser(response.data.user)
+            console.log(response.data.user)
+            
+        }).catch(err => {
+          console.log(err);
+        })
+    }
+
+    getUser()
+},[apiUrl])
 
   const logout = () =>{
     localStorage.removeItem('token')
@@ -20,8 +42,10 @@ function Main() {
           <div className='logo-container rounded-3 shadow'></div>
         </div>
 
-        <div className='info-section rounded shadow'>
-          <h3 className='p-0 m-1 text-center'>Bienvenue John Doe</h3>
+        <div className='info-section rounded shadow d-flex flex-column'>
+          <h3 className='p-0 m-1 text-center'>Bienvenue {`${user?.sexe == 'Masculin' ? 'Mr' : 'Mme'} ${user?.nom} ${user?.prenom}`}</h3>
+          <h3 className='p-0 m-1 text-center'>Profession : {`${user?.qualification} `}</h3>
+          {/* <h3 className='p-0 m-1 text-center'>Dernière Connexion : {`${user?.qualification} `}</h3> */}
         </div>
         <button className='btn btn-danger shadow logout' onClick={logout}>Se déconnecter <i class="fa-solid fa-right-from-bracket"></i></button>
     </div>
